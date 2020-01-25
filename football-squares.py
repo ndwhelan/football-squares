b4f21f90236ce3a1ba2efaa894c280aa9edab7da
Nick Whelan <nickw@indeed.com>
2020-01-24 19:21:56 -0600

    Undoing shuffling of the score values

--- football-squares.py
+++ football-squares.py
@@ -17,7 +17,7 @@ class PlayersAndCountsParamType(click.ParamType):
             player, count = value.split(",")
         except ValueError:
             self.fail(
-                f"Unable to parse name and number for \"{value}\". Are there two parts?",
+                f'Unable to parse name and number for "{value}". Are there two parts?',
                 param,
                 ctx,
             )
@@ -25,15 +25,12 @@ class PlayersAndCountsParamType(click.ParamType):
         try:
             return player, int(count)
         except ValueError:
-            self.fail(f"Count \"{count}\" is not a valid whole number")
+            self.fail(f'Count "{count}" is not a valid whole number')
 
 
 @click.command()
 @click.option(
-    "-t",
-    "--teams",
-    type=(str, str),
-    help="The two teams that are competing",
+    "-t", "--teams", type=(str, str), help="The two teams that are competing",
 )
 @click.option(
     "-p",
@@ -68,17 +65,13 @@ def build_squares(teams: Tuple[str, str], players: Iterable[Tuple[str, int]]) ->
     player_array_split: List[List[Union[str, int]]] = [
         player_array[i : i + 10] for i in range(0, len(player_array), 10)
     ]
+    # Need copy, because we insert for blank
     top_score_values: List[Union[str, int]] = SCORE_VALUES.copy()
-    side_score_values: List[int] = SCORE_VALUES.copy()
-
-    random.shuffle(top_score_values)
-    random.shuffle(side_score_values)
-
     top_score_values.insert(0, " ")  # Create a blank corner square
 
     for x in range(10):
         # Insert side scores
-        player_array_split[x].insert(0, side_score_values[x])
+        player_array_split[x].insert(0, SCORE_VALUES[x])
 
     player_array_split.insert(0, top_score_values)  # Insert top scores
 
